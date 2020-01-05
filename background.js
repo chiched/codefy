bba057c24100438bf8326340350b44d7b9370892
David Chiche <davidchiche@gmail.com>
2020-01-05 18:10:59 +0700

    Prettified the code

--- background.js
+++ background.js
@@ -1,38 +1,37 @@
-var setIcon = (tabId) => {
-  browser.browserAction.setIcon({path: "icons/favicon-activated-32x32.png", tabId: tabId});
-}
+var setIcon = tabId => {
+  browser.browserAction.setIcon({
+    path: "icons/favicon-activated-32x32.png",
+    tabId: tabId
+  });
+};
 
-var unsetIcon = (tabId) => {
-  browser.browserAction.setIcon({path: "icons/favicon-32x32.png", tabId: tabId});
-}
+var unsetIcon = tabId => {
+  browser.browserAction.setIcon({
+    path: "icons/favicon-32x32.png",
+    tabId: tabId
+  });
+};
 
-var sendClick = (tab) => {
-  browser.tabs.sendMessage(
-    tab.id,                   
-    {
-    action:  "buttonClick", 
-    tabId: tab.id 
-    }                                
-  );
+var sendClick = tab => {
+  browser.tabs.sendMessage(tab.id, {
+    action: "buttonClick",
+    tabId: tab.id
+  });
 };
 
 var checkStatus = (tab, changeInfo) => {
   if (changeInfo.status === "complete") {
-    browser.tabs.sendMessage(
-      tab,                   
-      {action:  "tabUpdated", tabId: tab }                                
-    );
+    browser.tabs.sendMessage(tab, { action: "tabUpdated", tabId: tab });
   }
-
 };
 
- var updateIcon = (response) => {
-  if (response.message === 'setIcon') {
+var updateIcon = response => {
+  if (response.message === "setIcon") {
     setIcon(response.tabId);
-  } else if (response.message === 'unSetIcon') {
+  } else if (response.message === "unSetIcon") {
     unsetIcon(response.tabId);
   }
-};  
+};
 
 browser.browserAction.onClicked.addListener(sendClick);
 browser.tabs.onUpdated.addListener(checkStatus);
--- end.js
+++ end.js
@@ -1,98 +1,89 @@
-
-let favUrl = '';
-let cssCodefied = '';
+let favUrl = "";
+let cssCodefied = "";
 const cssCollection = document.styleSheets;
 
-const codefyCss = (tabId) => { 
-    const link = document.createElement('link'); 
-    link.id = 'codefy'
-    link.rel = 'stylesheet';  
-    link.type = 'text/css'; 
-    link.href = browser.runtime.getURL("style.css");
-    for (let i = 1; i < cssCollection.length; i++) {
-        cssCollection[i].disabled = true;
+const codefyCss = tabId => {
+  const link = document.createElement("link");
+  link.id = "codefy";
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = browser.runtime.getURL("style.css");
+
+  for (let i = 1; i < cssCollection.length; i++) {
+    cssCollection[i].disabled = true;
+  }
+  document.documentElement.append(link);
+  cssCodefied = true;
+  let obj = { codefyValue: { [tabId]: true } };
+  browser.storage.local.set(obj);
+  setFavicon();
+  browser.runtime.sendMessage({
+    message: "setIcon",
+    tabId: tabId
+  });
+};
+
+const unCodefyCss = tabId => {
+  for (let i = 1; i < cssCollection.length; i++) {
+    cssCollection[i].disabled = false;
+  }
+  let element = document.getElementById("codefy");
+  element.parentNode.removeChild(element);
+  cssCodefied = false;
+  let obj = { codefyValue: { [tabId]: false } };
+  browser.storage.local.set(obj);
+  setFavicon();
+  browser.storage.local.get().then(function(value) {});
+  browser.runtime.sendMessage({
+    message: "unSetIcon",
+    tabId: tabId
+  });
+};
+
+const setFavicon = function() {
+  const nodeList = document.getElementsByTagName("link");
+  for (let i = 0; i < nodeList.length; i++) {
+    if (
+      nodeList[i].getAttribute("rel") == "icon" ||
+      nodeList[i].getAttribute("rel") == "shortcut icon"
+    ) {
+      if (cssCodefied === true) {
+        favUrl = nodeList[i].getAttribute("href");
+        nodeList[i].setAttribute(
+          "href",
+          browser.runtime.getURL("icons/favicon.ico")
+        );
+      } else {
+        nodeList[i].setAttribute("href", favUrl);
+      }
     }
-    document.documentElement.append(link); 
-    cssCodefied = true;
-    let obj = {codefyValue: {[tabId] : true}};
-    browser.storage.local.set(obj);
-    setFavicon();
-    // check value stored
-    browser.storage.local.get()
-    .then(function(value) {
-    });
-    browser.runtime.sendMessage({
-        message: "setIcon",
-        tabId: tabId
-      });
-}
+  }
+};
 
-const unCodefyCss = (tabId) => {
-    for (let i = 1; i < cssCollection.length; i++) {
-        cssCollection[i].disabled = false;
-    }
-    let element = document.getElementById('codefy');
-    element.parentNode.removeChild(element);
-    cssCodefied = false;
-    let obj = {codefyValue: {[tabId] : false}};
-    browser.storage.local.set(obj);
-    setFavicon();
-        // check value stored
-        browser.storage.local.get()
-        .then(function(value) {
-        });
-        browser.runtime.sendMessage({
-            message: "unSetIcon",
-            tabId: tabId
-          });
-}
-
-const setFavicon = function(){
-    const nodeList = document.getElementsByTagName("link");
-    for (let i = 0; i < nodeList.length; i++)
-    {
-        if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon")) {
-            if (cssCodefied === true) {
-                favUrl = nodeList[i].getAttribute("href");
-                nodeList[i].setAttribute("href", browser.runtime.getURL("icons/favicon.ico"));
-            } else {
-                nodeList[i].setAttribute("href", favUrl);
-            }
-        }
-    }     
-}
-
-const toggleCss = (tabId) => {
-    if (cssCodefied === '') {
-    browser.storage.local.get()
-    .then(function(response) {
-        if (response.codefyValue.tabId === true) {
-            unCodefyCss(tabId);
-        } else{
-            codefyCss(tabId);
-        }
-    });
-    } else if (cssCodefied === true){
+const toggleCss = tabId => {
+  if (cssCodefied === "") {
+    browser.storage.local.get().then(function(response) {
+      if (response.codefyValue.tabId === true) {
         unCodefyCss(tabId);
-    } else {
+      } else {
         codefyCss(tabId);
-    }
-}
-browser.runtime.onMessage.addListener((request) => {
-    // if (request.action === 'newTab') {
-    //     console.log('newTaaaaaab')
-    //     return Promise.resolve({response: cssCodefied});
-    // } 
-    if (request.action === 'buttonClick') {
-        toggleCss(request.tabId);
-    } else if (request.action === 'tabUpdated') {
-        browser.storage.local.get()
-        .then(function(response) {
-            let tabNumb = request.tabId;
-            if (response.codefyValue[tabNumb] === true) {
-                codefyCss(tabNumb);
-               
-            }
-        });
-    }
+      }
+    });
+  } else if (cssCodefied === true) {
+    unCodefyCss(tabId);
+  } else {
+    codefyCss(tabId);
+  }
+};
+browser.runtime.onMessage.addListener(request => {
+  if (request.action === "buttonClick") {
+    toggleCss(request.tabId);
+  } else if (request.action === "tabUpdated") {
+    browser.storage.local.get().then(function(response) {
+      let tabNumb = request.tabId;
+      if (response.codefyValue[tabNumb] === true) {
+        codefyCss(tabNumb);
+      }
+    });
+  }
 });
